8730481eac261192978dd8af3ed0de8d780e6a11
Almir Dessbesel <xstaybrutalx@live.com>
2023-10-26 19:33:15 -0300

    adcionando pessoa

---
+++ pessoa.py
@@ -0,0 +1,10 @@
+class Pessoa:
+
+    def __init__(self, nome, email, matricula, endereco):
+        self._nome = nome
+        self._email = email
+        self._matricula = matricula
+        self._endereco = endereco
+
+    def exibirNomeMatricula(self):
+        pass
